768eaaa07b71947050b4e009e85ca573fdced8c7
Serellka <vprogamervip@gmail.com>
2021-09-10 21:18:09 +0300

    feat: adds Person, ShopManager ans Exception Classes

---
+++ Shops/Entities/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace Shops.Entities
+{
+    public class Person
+    {
+        Person(string name, uint moneyAmount)
+        {
+            Name = name;
+            Balance = moneyAmount;
+        }
+
+        public string Name { get; }
+
+        public uint Balance { get; set; }
+    }
+}
---
+++ Shops/Services/ShopManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Shops.Entities;
+
+namespace Shops.Services
+{
+    public class ShopManager
+    {
+        private List<Product> _products;
+        private List<Shop> _shops;
+
+        public ShopManager()
+        {
+            _products = new List<Product>();
+            _shops = new List<Shop>();
+        }
+
+        public Shop Create(string name)
+        {
+            var shop = new Shop(name);
+            _shops.Add(shop);
+            return shop;
+        }
+
+        public Product RegisterProduct(string name)
+        {
+            var product = new Product(name);
+            _products.Add(product);
+            return product;
+        }
+    }
+}
