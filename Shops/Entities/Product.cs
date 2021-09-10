3c3a1839a9bfe98f997c43dd26bc43f01ed62d9a
Serellka <vprogamervip@gmail.com>
2021-09-10 21:16:43 +0300

    feat: adds Product class

---
+++ Shops/Entities/Product.cs
@@ -0,0 +1,32 @@
+﻿using Shops.Tools;
+
+namespace Shops.Entities
+{
+    public class Product
+    {
+        public Product(string name)
+        {
+            Count = 0;
+            Name = name;
+        }
+
+        public string Name { get; set; }
+
+        public int Count { get; private set; }
+
+        public void AddProduct(int delta)
+        {
+            Count += delta;
+        }
+
+        public void ReserveProduct(int delta)
+        {
+            if (delta > Count)
+            {
+                throw new ShopException("Invalid value of count");
+            }
+
+            Count -= delta;
+        }
+    }
+}
